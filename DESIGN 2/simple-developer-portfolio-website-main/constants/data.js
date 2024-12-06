6d1f7f8a0886ad4b8b30ddff60aa403f986c7baa
Anubhav Jaiswal <anubhavjiaswal1803@gmail.com>
2024-12-06 11:56:08 +0530

    another template

---
+++ DESIGN 2/simple-developer-portfolio-website-main/constants/data.js
@@ -0,0 +1,93 @@
+const userData = {
+  githubUsername: "anubhav12302387",
+  name: "Anubhav Jaiswal",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/pic.png.jpg",
+  email: "anubhavjaiswal1803@gmail.com",
+  phone: "+91 7980921124",
+  address: "Kolkata, West Bengal, India.",
+  projects: [
+    {
+      title: "Event Management System",
+      link: "https://awslpu.xyz",
+      imgUrl: "/awslpu.png",
+    },
+    {
+      title: "Portfolio Design",
+      link: "https://anubhavjaiswal.netlify.app/",
+      imgUrl: "/ttaj.png",
+    },
+    {
+      title: "AI Face Detection",
+      link: "",
+      imgUrl: "/aiface.jpg",
+    },
+  ],
+  about: {
+    title:
+      "I'm a Full Stack Web Developer that loves building Software products and web applications that impact millions of lives",
+    description: [
+      `I'm on a mission to create captivating and inspiring online experiences that leave a lasting impact. With each line of code I write, I'm excited to learn, grow, and push the boundaries of what's possible in this ever-evolving field.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a Pro. I've been coding in React, Vue, Node.js, Express, MongoDB, MySQL, Bootstrap, Tailwind, C, C++,PHP,Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "Event Management System",
+    currentProjectUrl: "https://awslpu.xyz",
+  },
+  experience: [
+    {
+      title: "Operations Lead",
+      company: "AWS Cloud Community",
+      year: "Sep 2023 - Present",
+      companyLink: "https://awslpu.xyz",
+      desc: "As the Operations Lead at AWS Cloud Club, Lovely Professional University, I oversee the seamless execution of AWS-focused events, workshops, and hackathons while coordinating cross-functional teams to foster cloud computing awareness and skill development. My role involves managing logistics and resources to enhance community engagement and event impact, along with building strategic partnerships and collaborations to drive participation and knowledge sharing.",
+    },
+    {
+      title: "Cyber Security Lead",
+      company: "Google Developer Student Club",
+      year: "Sep 2023 - Sep 2024",
+      companyLink: "https://github.com/gdsclpu",
+      desc: "As the Cyber Security Lead at Google Developer Student Club, Lovely Professional University, I focus on strengthening the club's cybersecurity initiatives through rigorous security testing and best practices. My role involves identifying vulnerabilities, ensuring secure development processes, and fostering a culture of cybersecurity awareness within the developer community. Additionally, I contribute to organizing workshops and events to educate peers about emerging trends and tools in cybersecurity.",
+    },
+    {
+      title: "Co - Lead",
+      company: "NOOB 4rmy",
+      year: "Sep 2023 - Jan 2024",
+      companyLink: "https://vulncon.in",
+      desc: "As the Co-Lead at NOOB 4rmy, I collaborate with a team of cybersecurity enthusiasts to conduct penetration testing, vulnerability assessments, and security research projects. My role involves managing team operations, coordinating training sessions, and organizing CTF competitions to enhance practical skills and knowledge in cybersecurity. Additionally, I contribute to developing open-source tools and resources to support the cybersecurity community and promote ethical hacking practices.",
+    },
+    {
+      title: "Technical Team Member",
+      company: "AWS Cloud Community",
+      year: "Oct 2022 - Sep 2023",
+      companyLink: "https://awslpu.xyz",
+      desc: "As a Technical Team Member at AWS Cloud Club, Lovely Professional University, I actively contributed to organizing cloud computing workshops, hackathons, and events to promote AWS services and technologies among students. My role involved developing technical content, conducting hands-on labs, and providing mentorship to help participants gain practical experience in cloud computing. Additionally, I collaborated with cross-functional teams to support event planning, logistics, and community engagement initiatives.",
+    },
+    {
+      title: "Technical Team Member",
+      company: "Google Developer Student Club",
+      year: "Sep 2022 - Sep 2023",
+      companyLink: "https://github.com/gdsclpu",
+      desc: "As a Technical Team Member at Google Developer Student Club, Lovely Professional University, I contributed to organizing technical workshops, hackathons, and events to promote Google technologies and platforms among students. My role involved developing technical content, conducting hands-on labs, and providing mentorship to help participants gain practical experience in web development, cloud computing, and mobile app development. Additionally, I collaborated with cross-functional teams to support event planning, logistics, and community engagement initiatives.",
+    },
+    {
+      title: "Technical Team Member",
+      company: "NOOB 4rmy",
+      year: "Oct 2021 - Sep 2022",
+      companyLink: "https://vulncon.in",
+      desc: "As a Technical Team Member at NOOB 4rmy, I participated in cybersecurity training sessions, CTF competitions, and security research projects to enhance my skills and knowledge in ethical hacking and penetration testing. My role involved collaborating with team members to solve challenges, analyze vulnerabilities, and develop practical solutions to secure systems and applications. Additionally, I contributed to developing open-source tools and resources to support the cybersecurity community and promote ethical hacking practices.",
+    },
+  ],
+  resumeUrl:
+    "#",
+  socialLinks: {
+    instagram: "https://instagram.com/anubhavjaiswal_",
+    twitter: "https://twitter.com/anubhavjaiswal_",
+    linkedin: "https://linkedin.com/in/anubhavjaiswal",
+    github: "https://github.com/",
+    // facebook: "https://facebook.com/",
+    // peerlist: "https://peerlist.io/manuarora",
+  },
+};
+
+export default userData;
